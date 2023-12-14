f98a3a349e6419925395aff21afb4fab098215d9
HoleHolo <hola0_o@qq.com>
2023-12-14 15:58:26 +0800

    ufshcd.h: add missing part

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -1704,7 +1704,14 @@ int ufshcd_read_unit_desc_param(struct ufs_hba *hba, int lun,
 int ufshcd_send_vendor_scsi_cmd(struct ufs_hba *hba,
 		struct scsi_device *sdp, unsigned char* cdb, void* buf);
 void ufshcd_set_auto_hibern8_delay(struct ufs_hba *hba, unsigned int value);
-inline u8 ufshcd_scsi_to_upiu_lun(unsigned int scsi_lun);
+inline u8 ufshcd_scsi_to_upiu_lun(unsigned int scsi_lun)
+{
+	if (scsi_is_wlun(scsi_lun))
+		return (scsi_lun & UFS_UPIU_MAX_UNIT_NUM_ID)
+			| UFS_UPIU_WLUN_ID;
+	else
+		return scsi_lun & UFS_UPIU_MAX_UNIT_NUM_ID;
+}
 int ufshcd_query_flag_retry(struct ufs_hba *hba, enum query_opcode opcode,
 			    enum flag_idn idn, bool *flag_res);
 int ufshcd_query_attr_retry(struct ufs_hba *hba, enum query_opcode opcode,
